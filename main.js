5c2a5794b80feb3c7ada111b250ad0d79fa6bb8b
Alan Neculpan <aneculpan_@estudiantesunap.cl>
2024-01-30 12:31:48 -0300

    avance en la logica de la aplicacion

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 
 function encriptarMensaje () {
+    let mensaje = document.getElementById('mensaje').value;
+    let texto_mensaje = mensaje.toLowerCase();
+    console.log(texto_mensaje);
+    
     
 }
 
