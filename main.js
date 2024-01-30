4ffda7bbfedc56c779effb28c0a9ee45c2a7e00c
Alan Neculpan <aneculpan_@estudiantesunap.cl>
2024-01-30 14:23:56 -0300

    funcionalidades y estilos  terminados

--- main.js
+++ main.js
@@ -1,13 +1,41 @@
 
-function encriptarMensaje () {
-    let mensaje = document.getElementById('mensaje').value;
-    let texto_mensaje = mensaje.toLowerCase();
-    console.log(texto_mensaje);
+eventoClickEncriptar();
+
+function encriptarMensaje () {    
+    let mensaje = document.getElementById('mensaje').value.toLowerCase();
     
-    
+    var textoEncriptado = mensaje.replace(/e/img, 'enter');
+    var textoEncriptado = textoEncriptado.replace(/o/img, 'ober');
+    var textoEncriptado = textoEncriptado.replace(/i/img, 'imes');
+    var textoEncriptado = textoEncriptado.replace(/a/img, 'ai');
+    var textoEncriptado = textoEncriptado.replace(/u/img, 'ufat');
+
+    document.getElementById('text_area').innerHTML = textoEncriptado;
 }
 
-function desencriptarMensaje () {
+function desEncriptarMensaje () {
+    let mensaje = document.getElementById('mensaje').value.toLowerCase();
     
+    var textoDesEncriptado = mensaje.replace(/enter/img, 'e');
+    var textoDesEncriptado = textoDesEncriptado.replace(/ober/img, 'o');
+    var textoDesEncriptado = textoDesEncriptado.replace(/imes/img, 'i');
+    var textoDesEncriptado = textoDesEncriptado.replace(/ai/img, 'a');
+    var textoDesEncriptado = textoDesEncriptado.replace(/ufat/img, 'u');
+
+    document.getElementById('text_area').innerHTML = textoDesEncriptado;
 }
 
+function copiarMensaje() {
+    var textArea = document.getElementById('text_area');
+    textArea.select();
+    document.execCommand('copy');
+}
+
+function eventoClickEncriptar() {
+    let botonCopiar = document.getElementById('btn_copiar');
+    let botonEncriptar = document.getElementById('btn_encriptar');
+
+    botonEncriptar.addEventListener('click', function() {
+        botonCopiar.style.display = 'block';
+    });
+}
